5aecd282182f54af2312a97f75b78410a8497e0c
IgorNogueiraPessoa <igornnpessoa@gmail.com>
2024-03-27 14:38:52 -0300

    Adicionando funções

--- resources/views/editar.blade.php
+++ resources/views/editar.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Laravel</title>
+    <title>Editar - Contato</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </head>
@@ -42,7 +42,7 @@
  
             <div class="row">
                 <div class="mb-3">
-                <form method="POST" action="/adicionarContato">
+                <form method="POST" action="/editar">
                  @csrf
                     <label for="exampleFormControlInput1" class="form-label">Nome:</label>
                     <input type="text" class="form-control" name="nome">
